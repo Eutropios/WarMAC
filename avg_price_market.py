1c2c602892a83d5a3180a2beff34925e97d05b77
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2023-02-22 00:55:15 -0800

    Massive overhaul to information fetching Switched from HTML scraping technique to API usage Changed colours on command line for exapmple usage Changed fetching library from urllib3 to requests Minor redundancy fixes

--- avg_price_market.py
+++ avg_price_market.py
@@ -1,37 +1,53 @@
 """
-Author: Noah J (Eutropios; Sink Cat)
+Warframe Market Average Price Calculator
+Retrieves the sell price from all listings of a given item from https://warframe.market for a
+specific platform, then finds the average price in platinum of the listings.
+
+Copyright (C) 2023  Noah Jenner
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+***I am NOT affiliated with Warframe, Digital Extremes and its subsidaries, Warframe.market, 
+Playstation, Sony and its subsidaries, XBOX, or Microsoft its subsidaries.***
+
 Date of Creation: January 22, 2023
-Date Last Modified: February 17, 2023
-Version 1.2.1
+Date Last Modified: February 21, 2023
+Version 1.3.0
 Version of Python built with: 3.11.2 (Not required, I believe 3.6 will suffice)
 Built in packages required: json, datetime
 External packages required: urllib3, colorama, beautifulsoup4
 
-Scrapes the sell price from all listings of a given item from https://warframe.market,
-then finds the average of the remaining listings.
-
-THIS PROJECT IS STILL IN DEVELOPMENT. Additional error handling, expansion of time lookup dates,
-and deleting lowest/highest sell prices to keep average in line are coming soon.
-I recognize that this isn't the most efficient method of obtaining prices. Warframe.market has
-an API that can be accessed, which would increase efficiency.
-
-***I am NOT affiliated with Warframe, Digital Extremes and its subsidaries, Warframe.market, 
-Playstation, Sony and its subsidaries, XBOX, or Microsoft its subsidaries.***
+TO ADD:
+    THIS PROJECT IS STILL IN DEVELOPMENT. Additional error handling, expansion of time lookup dates,
+    and deleting lowest/highest sell prices to keep average in line are coming soon.
 """
 
 import json
 from datetime import datetime, timezone
-from urllib.request import urlopen, Request
+import requests as rq
 from colorama import init as colorama_init, deinit as colorama_deinit
 from colorama import Fore, Style
-from bs4 import BeautifulSoup as bs
 
 PLATFORMS = {"xbox", "switch", "ps4", "pc"}
 PLATFORM_DICT = {"xbox one":"xbox", "xbox series x":"xbox", "xbox series s":"xbox",
             "nintendo switch":"switch", "oled switch":"switch", "switch lite":"switch",
-            "playstation":"ps4", "playstation 4":"ps4", "playstation 5":"ps4", "ps5":"ps4"}
+            "nintendo switch lite":"switch", "playstation":"ps4", "playstation 4":"ps4",
+            "playstation 5":"ps4", "ps5":"ps4", "ps":"ps4"}
+API_ROOT_LINK = "https://api.warframe.market/v1/items"
+headers={'User-Agent': 'Mozilla', 'Content-Type': 'application/json'}
 
 def get_platform():
+    """Gets the platform of the user and adds it to the HTTP request header"""
     colorama_init()
     platform = ""
     while True:
@@ -41,46 +57,39 @@ def get_platform():
         platform = str(input()).lower().strip()
         if platform in PLATFORMS:
             break
-        elif platform in PLATFORM_DICT:
+        if platform in PLATFORM_DICT:
             platform = platform.replace(platform, PLATFORM_DICT[platform])
             break
         print(f"Sorry, {platform} is not a valid entry.")
-
-    if platform != "pc":
-        return platform + "."
-    else:
-        return ""
+    headers['Platform'] = platform
 
 def get_input():
     """Gets item user wants in a colourful way."""
     print("What part would you like to find the price of? " +
-            f"Please use the form of {Fore.GREEN}\"Braton Prime Set\"{Style.RESET_ALL}: ")
+            f"Please use the form of {Fore.CYAN}\"Braton Prime Set\"{Style.RESET_ALL}: ")
     name_of_item = str(input()).lower().strip()
     return name_of_item
 
-def url_of_item(platform, name_of_item):
+def url_of_item(name_of_item):
     """Fixes the string of a user input so it can be used in a URL."""
     name_of_item = name_of_item.replace("&", "and")
     name_of_item = name_of_item.replace(" ", "_")
-    url_to_scrape = "https://" + platform + "warframe.market/items/" + name_of_item
-    return url_to_scrape
+    listings_url = API_ROOT_LINK + "/" + name_of_item + "/orders"
+    return listings_url
 
 def output_message(item, avg):
     """Outputs the average platinum for the specified item in a colourful way."""
-    print(f"The going rate for a {Fore.GREEN}{item}{Style.RESET_ALL} " +
-            f"is {Fore.GREEN}{avg}{Style.RESET_ALL}.")
+    print(f"The going rate for a {Fore.CYAN}{item}{Style.RESET_ALL} " +
+            f"is {Fore.CYAN}{avg}{Style.RESET_ALL}.")
 
 def logic():
     """Logic of the program"""
-    platform, get_item = get_platform(), get_input()
-    url_to_scrape = url_of_item(platform, get_item)
-    page = urlopen(Request(url_to_scrape, headers={'User-Agent': 'Mozilla'}))
-    page_decoded = bs(page.read().decode('utf-8'), "lxml")
-    found_em = page_decoded.find("script", attrs = {"id": "application-state"})
-    em_decoded = found_em.decode(None, "utf-8")
-    no_prefix = em_decoded.removeprefix('<script id="application-state" type="application/json">')
-    stripped_garbage = json.loads(no_prefix.removesuffix('</script>'))
-    item_list = stripped_garbage['payload']['orders']
+    get_platform()
+    get_item = get_input()
+    listings_url = url_of_item(get_item)
+    page = rq.get(listings_url, headers=headers, timeout=5)
+    pulled_listings = json.loads(page.text)
+    item_list = pulled_listings['payload']['orders']
     now = datetime.now(timezone.utc)
     year, month = now.strftime("%Y"), now.strftime("%m")
 
@@ -108,7 +117,7 @@ def main():
         logic()
         print("Would you like to find the average price of another item? (Y/N)")
         user_inp = str(input()).lower()
-        if user_inp == "no" or user_inp == "n":
+        if user_inp in ("no", "n"):
             break
     print("Thanks for using this script!")
     colorama_deinit()
