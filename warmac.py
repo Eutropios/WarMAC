2cdaddf70e4d704ba637e1f0d8f89516ced4344a
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2023-04-08 20:54:52 -0700

    Fixed accidental errors

--- warmac.py
+++ warmac.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 """
 Allows for program execution without calling python3
+Detailed program introduction found in main.py
+Formal documentation WIP
 """
+
 from WarMAC import main
 if __name__ == "__main__":
     try:
