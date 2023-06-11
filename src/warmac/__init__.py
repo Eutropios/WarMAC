59148e9cb8c233802ede9324f8df390bcb2ca47d
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2023-06-11 00:12:07 -0700

    Fixed code of conduct

--- src/warmac/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Main warmac files
-~~~
-
-* Any functions, classes, or variables used within this package are subject to change.
-Please do not use these in your own tools, as they will likely break in the future.
-"""  # noqa: D205,D400
